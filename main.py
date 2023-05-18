453d8b95bbfb1ce694c4dd04f9ee7ad5b07c25f8
Raul <qrananico25@gmail.com>
2023-05-18 20:09:04 -0300

    adicionei clique do mouse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from tkinter import simpledialog
 
 pygame.init()
 
@@ -14,17 +15,34 @@ white = (255,255,255)
 clock = pygame.time.Clock()
 tela = pygame.display.set_mode(tamanho)
 
+#nome e icone
 pygame.display.set_caption("Space Marker")
 
-
+#variáveis
 running = True
+
+#dicionarios
+estrelas = {}
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
-    
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            item = simpledialog.askstring("Space", "Nome da Estrela: ")
+            print(item)
+            if item == None:
+                item = "Desconhecido" + str(pos)
+            estrelas[item] = pos  #arrumar a biblioteca
+            print(estrelas)
+
+
+
+
+    #mostra na tela
     tela.fill(black)
     
     pygame.display.update()
