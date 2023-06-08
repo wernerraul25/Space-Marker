2be9c9e98f6d3c6bb639690f8959436e4e77d0ae
Raul <qrananico25@gmail.com>
2023-06-08 14:04:03 -0300

    evolução no projeto
    salva em dicionário.
    mostra o nome e a estrela na tela.
    NÃO mostra a linha.
    PROBLEMA: se o nome da estrela for igual a alguma anterior, substituirá a posição da estrela

--- main.py
+++ main.py
@@ -29,75 +29,73 @@ pygame.display.set_icon(icone)
 
 #fonte
 fonte_estrela = pygame.font.Font(None,24)
-fonte = pygame.font.Font(None,30)
+fonte = pygame.font.Font(None,28)
 
 #variáveis
 running = True
-x_y = (0,0)
-pos_diminuir = 25
-nome_estrela = None
+posicao = (0,0)
+nome = None
 mostra_marcacao = True
 
 #dicionarios
-posicoes = {}
+estrelas = {}
+nomes = []
 
 #funções
 #função para salvar as posições em um arquivo json
 def salva_posicao():
-     with open("posicoes.json","w") as file:
-          json.dump(posicoes,file)
+     with open("estrelas.json","w") as file:
+          json.dump(estrelas,file)
 #função para carregar as posições em um arquivo json
 def carrega_posicao():
-    global posicoes
+    global estrelas
     try:
-        with open("posicoes.json","r") as file:
-            posicoes = json.load(file)
+        with open("estrelas.json","r") as file:
+            estrelas = json.load(file)
     except:
-        with open("posicoes.json","w") as file:
-            json.dump(posicoes,file)
+        with open("estrelas.json","w") as file:
+            json.dump(estrelas,file)
 #função para excluir todas as posições da tela
 def exclui_posicao():
-     posicoes.clear()
+     estrelas.clear()
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            salva_posicao()
             running = False
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            salva_posicao()
             running = False
-        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1: #só funciona com botão esquerdo do mouse;)
-                    x_y = pygame.mouse.get_pos()
-                    nome_estrela = simpledialog.askstring("Space", "Nome da Estrela:")
-                    if nome_estrela == "":
-                        nome_estrela = "Desconhecido"
-                    elif nome_estrela: #vai que
-                         nome_estrela = nome_estrela
-                    elif nome_estrela is None:
-                         continue
-                    posicoes[nome_estrela] = x_y
-                    print(posicoes)
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1: #só funciona com botão esquerdo do mouse
+            posicao = pygame.mouse.get_pos()
+            nome = simpledialog.askstring("Space", "Nome da Estrela:")
+            if nome == "":
+                nome = "Desconhecido"
+            elif nome: #vai que
+                    nome = nome
+            elif nome is None:
+                    continue
+            nomes.append(nome)
+            estrelas[nome]= posicao
+            print(estrelas)
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_F10:
             salva_posicao()
-            #salva_nome()
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
             carrega_posicao()
-            #carrega_nome()
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_F12:
             exclui_posicao()   #apaga da tela as marcações e limpa o dicionario
-            #exclui_nome()
 
     #mostra na tela
     tela.blit(fundo, (0,0)) #fundo
-    pos_texto = tuple(valor - pos_diminuir for valor in (x_y)) #posição do texto acima da estrela
+    #pos_texto = tuple(valor - pos_diminuir for valor in (posicao)) #posição do texto acima da estrela
 
-    #ele pega a chave do dicionario, por isso o erro
-    #for chave in posicoes:
-        #print(chave)
-    
     #percorre as chaves(estrelas) do dicionário
-    for chave in posicoes:
-        posicao = posicoes[chave] #pega a posição de cada estrela(chave)
+    for chave in estrelas:
+        posicao = estrelas[chave]
         pygame.draw.circle(tela,white,(posicao),5)
+        nome_estrela = fonte_estrela.render(chave + str(posicao),True,white)
+        tela.blit(nome_estrela,(posicao))
 
     #mostra os F10,F11,F12
     texto_f10 = fonte.render("Pressione F10 para salvar as marcações",True,white)
